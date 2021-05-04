f697d822adcf989d9b799d1a397c0108c917e887
Tuan Thai <minhtuanthaivn@gmail.com>
2021-05-04 15:30:04 +0700

    Update flask-docker.py

--- flask-docker.py
+++ flask-docker.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Changed it up a bit!again, hello hello'
+    return 'Changed it up a bit!again, hello hello, hello FOSS
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
